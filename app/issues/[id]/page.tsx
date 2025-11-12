07b0357486bb6f08d9c25d936ef68b134a6e4c3d
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-11-12 09:10:35 +0530

    add styles and markdown to issue details

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,8 @@
+import IssueStatus from "@/app/components/IssueStatus";
 import prisma from "@/lib/prisma";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -14,11 +17,20 @@ const IssueDetailsPage = async ({ params }: Props) => {
   });
   if (!issue) notFound();
   return (
-    <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+    <div className="max-w-3xl space-y-3">
+      <div className="mb-5">
+        <Heading as="h2">Issue Details</Heading>
+      </div>
+      <div className="mb-3">
+        <Heading>{issue.title}</Heading>
+      </div>
+      <Flex gap="3">
+        <IssueStatus status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
