c916e727c3832a5fc429694fc2f2759a2daf4395
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-11-13 22:20:13 +0530

    add user login with google

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,42 +1,65 @@
 "use client";
 
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   const currentPath = usePathname();
-
+  const { status, data: session } = useSession();
   const links = [
     { href: "/dashboard", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
   ];
   return (
     <div>
-      <nav className="flex space-x-8 mb-5 border-b p-5 items-center border-green-400">
-        <Link
-          href="/"
-          className={`${
-            currentPath === "/" ? "text-green-400" : "text-green-600"
-          } text-lg`}
-        >
-          {<AiFillBug />}
-        </Link>
-        <div className="flex space-x-8">
-          {links.map((link, index) => (
+      <nav className="mb-5 border-b p-5 border-green-400">
+        <Flex direction="row" gap="5" justify="between">
+          <Flex gap="5" align="center">
             <Link
-              key={index}
-              href={link.href}
+              href="/"
               className={`${
-                currentPath === link.href
-                  ? "text-green-400 underline underline-offset-8"
-                  : "text-green-600"
-              } transition-colors text-lg font-bold hover:text-green-400`}
+                currentPath === "/" ? "text-green-400" : "text-green-600"
+              } text-lg`}
             >
-              {link.label}
+              {<AiFillBug />}
             </Link>
-          ))}
-        </div>
+            {links.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className={`${
+                  currentPath === link.href
+                    ? "text-green-400 underline underline-offset-8"
+                    : "text-green-600"
+                } transition-colors text-lg font-bold hover:text-green-400`}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link
+                href="/api/auth/signout"
+                className="hover:text-green-400 text-green-600 font-bold text-lg"
+              >
+                Log Out
+              </Link>
+            )}
+
+            {status === "unauthenticated" && (
+              <Link
+                href="/api/auth/signin"
+                className="hover:text-green-400 text-green-600 font-bold text-lg"
+              >
+                Log In
+              </Link>
+            )}
+          </Box>
+        </Flex>
       </nav>
     </div>
   );
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+ 
+export const handler= NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  })],
+  session:{
+    strategy:'jwt'
+  }
+})
+
+export {handler as GET, handler as POST}
